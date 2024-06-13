db115cadba39852709ca54e0a7fa95d2ac729cdf
Fozia1910 <115180350+Fozia1910@users.noreply.github.com>
2024-06-13 01:30:31 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+  description = "The Azure subscription ID"
+  default     = "005ccd00-654a-48a2-a3ef-ee990b504e9b"
+}
+
+variable "client_id" {
+  description = "The client ID for the Azure Service Principal"
+  default     = "97e3f051-e3db-4bb4-87b0-207f965750c6"
+}
+
+variable "client_secret" {
+  description = "The client secret for the Azure Service Principal"
+  default     = "XGe8Q~MOId-hIomQ5GtzebHlu_WqvYjRcW4FWaOT"
+}
+
+variable "tenant_id" {
+  description = "The tenant ID for the Azure Service Principal"
+  default     = "bf4a0561-a67c-4f99-99d6-4126cc81f773"
+}
