7024983311b3694651c47bc14e293987cb381e94
Fozia1910 <115180350+Fozia1910@users.noreply.github.com>
2024-06-13 16:57:37 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,35 +4,9 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                = "tf-example-storage"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  name                = "your-unique-storage-account-name"
+  resource_group_name = "your-resource-group-name"
+  location            = "East US"
   account_tier        = "Standard_LRS"
   account_replication_type = "LRS"
-}
-
-resource "azurerm_container_registry" "acr" {
-  name                = "tf-example-acr"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku {
-    name = "Standard"
-  }
-}
-
-resource "azurerm_public_ip_address" "pip" {
-  name                = "tf-example-pip"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  allocation_method  = "Dynamic"
-}
-
-resource "azurerm_app_service_plan" "asp" {
-  name                = "tf-example-asp"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
 }
